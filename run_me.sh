30a16e33bb38a3990cf293f12750391f1f33b723
Darci K <darcikhey27@gmail.com>
2016-11-27 23:27:07 -0800

    final script edit

--- run_me.sh
+++ run_me.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Darci K Saucedo
+# 
+# script to download multiple repos and so we can install an awesome vim configuration
+#
+#
 
 # first lets check if git is installed, if it is not let's go ahead and install it
 str=$(git --version)
@@ -11,25 +16,34 @@ if [[ "${str}" != *"git version"*  ]] ; then
     echo 
     echo "finished installing git"
 fi
+
 # git is installed
 echo "good!, git is installed"
+echo
 cd ~
-echo "creating folders"
-#mkdir .vim
-#mkdir .vim/plugin
-#mkdir .vim_runtime
+echo "creating folders .vim, .vim/plugin, .vim_runtime"
+mkdir .vim
+mkdir .vim/plugin
+mkdir .vim_runtime
 
 echo "downloading repo from github"
-#git clone https://github.com/amix/vimrc.git
+echo 
+git clone https://github.com/amix/vimrc.git
+cp -r vimrc/* .vim_runtime
 
+echo
 echo "running installation script"
-#sh ~/.vim_runtime/install_awesome_vimrc.sh
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo
+echo
 
 echo "downloading and installing paren complete"
-#git clone git://github.com/jiangmiao/auto-pairs.git
-#cp plugin/auto-pairs.vim ~/.vim/plugin
-#apt-get install git
-
+git clone git://github.com/jiangmiao/auto-pairs.git
+cp -r auto-pairs/plugin/auto-pairs.vim ~/.vim/plugin
+echo
 echo "installation complete"
+echo 
 echo "enjoy a simple but awesome vim installation by Darci K (mind wide open)"
 echo "adios"
+echo 
+echo 
