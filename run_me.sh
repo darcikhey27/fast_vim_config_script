7274df65bfaff095960bf6ef11166956d20a8656
darci <darcikhey27@gmail.com>
2018-08-05 21:00:19 -0700

    added options

--- run_me.sh
+++ run_me.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Darci K Saucedo
-# 
+#
 # script to download multiple repos and install a producitve VIM enviroment
 #
 filename=$0
@@ -16,36 +16,43 @@ then
 fi
 
 
-# first lets check if git is installed, if it is not let's go ahead and install it
+# check if git is installed, if it is not let's go ahead and install it
 str=$(git --version)
 str2="git version"
-if [[ "${str}" != *"git version"*  ]] ; then
+if [[ "${str}" != *"git version"*  ]]
+then
     echo "git is not installed"
-    echo "downloading git with sudo apt-get install git"
+    echo "Installing git"
     sudo apt-get update
     sudo apt-get install git
-    echo 
+    echo
     echo "finished installing git"
 fi
-
 # git is installed
 echo "good!, git is installed"
 echo
+
+echo "updating VIM"
+if [[ $params == "debian" ]]
+then
+    sudo add-apt-repository ppa:jonathonf/vim
+    sudo apt update
+    sudo apt install vim
+else
+    echo "is fedora based"
+    echo
+fi
+
+# begin installation of pluggins
 cd ~
 echo "creating folders .vim, .vim/plugin, .vim_runtime"
 mkdir .vim
 mkdir .vim/plugin
 mkdir .vim_runtime
 
-echo "updating VIM"
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt update
-sudo apt install vim
 
-
-
-echo "downloading repos from github"
-echo 
+echo "downloadking repos from github"
+echo
 git clone https://github.com/amix/vimrc.git
 cp -r vimrc/* .vim_runtime
 
@@ -59,11 +66,29 @@ echo "downloading and installing paren complete"
 git clone git://github.com/jiangmiao/auto-pairs.git
 cp -r auto-pairs/plugin/auto-pairs.vim ~/.vim/plugin
 echo
+echo "Done installing pluggins"
+echo "Customizing vimrc file"
+
+# modify .vimrc file
+options=(
+    "set t_Co=256"
+    "set background=light"
+    "set number"
+    "set mouse=a"
+    "set tags=tags"
+    ":imap jk <Esc>"
+    )
+for i in "${options[@]}"
+do
+    echo "$i" >> ~/options.txt
+done
+
 echo "installation complete"
-echo 
+echo
 echo "enjoy a simple but awesome vim installation by Darci K"
 echo "adios"
-echo 
-echo 
+echo
+echo
 echo "please restart your terminal"
 echo
+
